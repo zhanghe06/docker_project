6729038e23e5fd902c38a37d4cda2035e860c016
Zhang He <zhang_he06@163.com>
2017-05-15 15:57:49 +0800

    更新marriadb容器配置

--- marriadb/README.md
+++ marriadb/README.md
@@ -3,3 +3,9 @@ https://hub.docker.com/_/mariadb/
 ```
 $ sudo docker pull mariadb:10.1.23
 ```
+
+环境变量 | 说明
+--- | ---
+MYSQL_ROOT_PASSWORD | 创建root用户密码
+MYSQL_USER | 创建用户
+MYSQL_PASSWORD | 创建用户密码
--- marriadb/docker_run.sh
+++ marriadb/docker_run.sh
@@ -5,6 +5,8 @@ docker run \
     --name mariadb \
     -v `pwd`/data:/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD='123456' \
+    -e MYSQL_USER='www' \
+    -e MYSQL_PASSWORD='123456' \
     -p 3306:3306 \
     -d \
-    mariadb:10.1.23
+    mariadb:10.1.23 --log-bin --binlog-format=MIXED
