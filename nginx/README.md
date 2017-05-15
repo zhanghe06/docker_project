56c92ba5a53fe290441d36400f2b0d127045ef55
Zhang He <zhang_he06@163.com>
2017-05-15 14:24:44 +0800

    新增nginx容器

--- nginx/README.md
+++ nginx/README.md
@@ -3,3 +3,6 @@ https://hub.docker.com/_/nginx/
 ```
 $ sudo docker pull nginx:1.13.0
 ```
+
+
+The :ro option causes these directors to be read only inside the container.
--- nginx/docker_run.sh
+++ nginx/docker_run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 docker run \
+    -h nginx \
     --name nginx \
-    -v /some/content:/usr/share/nginx/html:ro \
+    -v `pwd`/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
+    -v `pwd`/conf/ssl:/etc/nginx/ssl:ro \
+    -v `pwd`/conf/conf.d:/etc/nginx/conf.d:ro \
+    -v `pwd`/log:/var/log/nginx \
+    -p 80:80 \
+    -p 443:443 \
     -d \
     nginx:1.13.0
