f9cae5afa58b458135d7e7d2b5012e8f15aa767d
Zhang He <zhang_he06@163.com>
2017-06-11 23:49:58 +0800

    更新redis容器配置

--- redis/README.md
+++ redis/README.md
@@ -6,3 +6,6 @@ $ sudo docker pull redis:3.2.8
 
 --protected-mode no
 
+持久化的两种方案：
+- rdb
+- aof
--- redis/docker_run_rdb.sh
+++ redis/docker_run_rdb.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[ -d ${PWD}/data ] || mkdir -p ${PWD}/data
+
 docker run \
     -h redis \
     --name redis \
@@ -7,4 +9,4 @@ docker run \
     -p 6379:6379 \
     -d \
     redis:3.2.8 \
-    redis-server --appendonly yes
+    redis-server
